02c01ddff63dda0842cd434398eefa0ebcde4f18
actualChandler <chambler97@gmail.com>
2017-11-08 10:23:51 -0700

    fixed links

--- src/components/Catalogs/Catalogs.js
+++ src/components/Catalogs/Catalogs.js
@@ -11,18 +11,26 @@ export default class Catalogs extends Component{
             </Row>
             <Row>
                <Col md={ 4 } mdOffset={ 2 }>
+               <a href="http://companycasuals.com/coverup/start.jsp" target="_blank" rel="noopener noreferrer">
                   <img src={ require('../../img/sanmar.jpg') } alt="SanMar" className="catalog_img"/>
+               </a>
                </Col>
                <Col md={ 4 }>
-                  <img src={ require('../../img/otto.jpg') } alt="Otto Cap" className="catalog_img"/>
+                  <a href="http://ottocap.com/" target="_blank" rel="noopener noreferrer">
+                     <img src={ require('../../img/otto.jpg') } alt="Otto Cap" className="catalog_img"/>
+                  </a>
                </Col>
             </Row>
             <Row>
                <Col md={ 4 } mdOffset={ 2 }>
-                  <img src={ require('../../img/alphaborder.jpg') } alt="AlphaBroder" className="catalog_img"/>
+                  <a href="http://www.4logowearables.com/coverupemb" target="_blank" rel="noopener noreferrer">
+                     <img src={ require('../../img/alphaborder.jpg') } alt="AlphaBroder" className="catalog_img"/>
+                  </a>
                </Col>
                <Col md={ 4 }>
-                  <img src={ require('../../img/augusta.jpg') } alt="SanMar" className="catalog_img"/>
+                  <a href="https://www.augustasportswear.com/" target="_blank" rel="noopener noreferrer">
+                     <img src={ require('../../img/augusta.jpg') } alt="SanMar" className="catalog_img"/>
+                  </a>
                </Col>
             </Row>
          </Grid>
